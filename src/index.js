252f47fe86c3e197999427a432f80475c6d67276
Ksenia Kuntsevich <kse.kuntsevich@icloud.com>
2019-09-30 17:28:00 +0300

    feat: add zeros task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function zeros(expression) {
   // your solution
+
+    let sum = expression.split('*').map(numberCoded => {
+        numberCoded = numberCoded.split(/(!+)/);
+        return interpreter(numberCoded[1])(+numberCoded[0]);
+    }).reduce((sum, num) => sum * num)
+     
+    let splitedByZeros = sum.toString().split(/(.*)[1-9]/)   
+
+    return splitedByZeros[splitedByZeros.length - 1].length;
+
 }
+
+
+function fact(num) {
+    num = BigInt(num)
+    return num <= 1n ? num : num * fact(num - 1n)
+}
+
+function multiplying(num) {
+    num = BigInt(num)
+    return num < 2n ? 1n : num * multiplying(num - 2n)
+}
+
+let interpreter = (code) => (code === "!" ?  fact :  multiplying);
